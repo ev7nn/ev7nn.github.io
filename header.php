d31081250c6d66c4ab3cc5cd54b0dc7b467554e3
ev7nn <138921877+ev7nn@users.noreply.github.com>
2025-08-06 10:58:09 -0400

    Create header.php

---
+++ header.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<!-- favicon for tab -->
+<link rel="icon" type="image/png" sizes="16x16" href="https://i.imgur.com/YlhsH4B.png">
+<title></title>
+<style>
+    .topnav {
+        background-color: #FFFFFF;
+        list-style-type: none;
+        padding: 20;
+        width: 96%;
+        margin: 2%;
+    }
+
+</style>
+<body>
+<!-- HEADER -->
+    <div class="topnav" style="text-align: center">
+        <!-- LOGO HOME -->
+        <a href="https://www.aevn.digital">
+            <img src="https://i.imgur.com/cXY6FK2.gif" alt="logo" width="300px" height="90px" />
+        </a>
+
+        <!-- BANNER HOME -->
+        <!--<img src="https://i.imgur.com/j1lKvuP.png" alt="banner" style="background-color: #ffffff; width: 300px; height: 80px;" /><br>-->
+        <br>
+        <!-- LINES HOME -->
+        <a href="play">
+            <img src="https://i.imgur.com/7RcDxuo.png" alt="games" width="100" height="50" />
+        </a>
+        <a href="https://soundcloud.com/aevndigital">
+            <img src="https://i.imgur.com/484hWrB.png" alt="music" width="100" height="50" />
+        </a>
+        <a href="https://www.instagram.com/aevndigital/">
+            <img src="https://i.imgur.com/sjhZjjc.png" alt="contact" width="100" height="50" />
+        </a>
+        <a href="https://t.vndb.org/ch/03/78003.jpg">
+            <img src="https://i.imgur.com/8z5x0TN.png" alt="about" width="100" height="50" />
+        </a>
+    </div>
+<!-- MAIN --><!-- MAIN --><!-- MAIN --><!-- MAIN --><!-- MAIN --><!-- MAIN --><!-- MAIN --><!-- MAIN --><!-- MAIN -->
+<!-- OTHER -->
+    <script>
+        const titles = ['aevndigital: even digitaller', 'aevndigital: the website', 'aevndigital'];
+        document.title = titles[Math.floor(Math.random() * titles.length)];
+    </script>
+</body>
+</html>
